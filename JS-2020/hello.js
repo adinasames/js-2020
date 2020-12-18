9952faff000c4f9f6654f83d2818e3f666f34a72
adina.sames <adina.sames@gmail.com>
2020-12-18 20:53:51 +0200

    JS-class 1

---
+++ JS-2020/hello.js
@@ -0,0 +1,22 @@
+
+var time = 3000
+time = time + 1000
+var message = 'Bine ai venit'
+var isLate = true
+
+setTimeout(welcome, time)
+
+function welcome() {
+    alert(message)
+    alert("Welcome!")
+}
+
+if (isLate) {
+    alert("Go Home")
+}
+
+if (isLate) {
+    alert("Go Home")
+} else {
+    alert("continue")
+}
