81ca315f42afae5dae3f1e0a4586239c3081e1b9
Adina Sames <adina.sames@gmail.com>
2021-02-12 19:18:55 +0200

    speed game

--- speed/speed.js
+++ speed/speed.js
@@ -5,6 +5,8 @@ startButton.addEventListener("click", startGame)
 
 function startGame() {
     startButton.classList.add("hidden")
+    let interval = 1000
+    setInterval(createNewLetter, interval);
 }
 
 
@@ -21,4 +23,35 @@ function randomLetter() {
 }
 
 
-function 
+function randomColor() {
+    let red = randomNumber(255)
+    let green = randomNumber (255)
+    let blue = randomNumber (255)
+
+    return `rgb(${red},${green},${blue})`
+}
+
+
+function randomPosition() {
+    let position = randomNumber(90)
+    return `${position}%`
+}
+
+function createNewLetter() {
+    let letter = randomLetter
+    let color = randomColor
+    let top = randomPosition
+    let right = randomPosition
+    let div = document.createElement("div")
+    div.classList.add("letter")
+    // setam textul din interiorul div-ului (litera aleatorie)
+    div.innerText = letter
+    div.style.backgroundColor = color
+    div.style.top = top
+    div.style.right = right
+    // setam o clasa cu valoarea literei aleatorii
+    div.classList.add("letter")
+    //adaugam elementul div in body
+    document.querySelector("body").appendChild(div)
+}
+
